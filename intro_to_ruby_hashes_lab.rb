7e4bd8ffbab2d0318bbd2694d08b8e306effd720
Johnson Kow <jkow95@gmail.com>
2020-02-03 18:09:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
       },
       :names=>
       {
-      :reading_railroad => "",
+      :reading_railroad => Hash.new,
       :pennsylvania_railroad => "",
       :b_and_o_railroad => "",
       :shortline => "",
