54338f286b95ab021804459608846cd1c24de10d
Johnson Kow <jkow95@gmail.com>
2020-02-03 17:51:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,13 @@ def monopoly_with_fourth_tier
   {
     :railroads => 
     {
+      :names =>
+      {
+        :reading_railroad=>{"mortgage_value"=>"$100"},
+      :pennsylvania_railroad=>{"mortgage_value"=>"$200"},
+      :b_and_o_railroad=>{"mortgage_value"=>"$400"},
+      :shortline=>{"mortgage_value"=>"$800"}
+      },
       :pieces => 4, 
       :rent_in_dollars => 
       {
@@ -34,25 +41,6 @@ def monopoly_with_fourth_tier
         :two_pieces_owned=>50,
         :three_pieces_owned=>100,
         :four_pieces_owned=>200
-      }
-      :names =>
-      {
-        :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
-      :pennsylvania_railroad=>
-      {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
       }
     }
   }
